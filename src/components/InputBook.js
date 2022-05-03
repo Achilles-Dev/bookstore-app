c9ebe6767f4c5f17171c0fa4be58c6c4a5cfe5fc
Solomon Achilles <solohag92@gmail.com>
2022-05-03 10:18:09 +0000

    Display added book

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const InputBook = () => (
-  <form className="form-container">
-    <input
-      type="text"
-      className="input-text"
-      placeholder="Add title..."
-      name="title"
-    />
-    <input
-      type="text"
-      className="input-text"
-      placeholder="Add author..."
-      name="author"
-    />
-    <button type="submit" className="input-submit">ADD BOOK</button>
-  </form>
-);
+const InputBook = () => {
+  const [book, setBook] = useState({
+    title: '',
+    author: '',
+  });
+  const dispatch = useDispatch();
+  const onTitleChange = (e) => setBook({ ...book, title: e.target.value });
+  const onAuthorChange = (e) => setBook({ ...book, author: e.target.value });
+  const handleClick = () => {
+    if (book.author && book.title) {
+      book.id = uuidv4();
+      dispatch(addBook(book));
+      setBook({
+        title: '',
+        author: '',
+      });
+    }
+  };
+
+  return (
+    <form className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add title..."
+        name="title"
+        value={book.title}
+        onChange={onTitleChange}
+      />
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add author..."
+        name="author"
+        value={book.author}
+        onChange={onAuthorChange}
+      />
+      <button
+        type="submit"
+        className="input-submit"
+        onClick={(e) => {
+          e.preventDefault();
+          handleClick();
+        }}
+      >
+        ADD BOOK
+      </button>
+    </form>
+  );
+};
 
 export default InputBook;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,27 +1,41 @@
 const ADD_BOOK = 'bookstore-app/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore-app/books/REMOVE_BOOK';
 
-const initialState = {
-  books: [],
-};
+const initialState = [
+  {
+    id: '1',
+    author: 'Solo',
+    title: 'First book',
+  },
+  {
+    id: '2',
+    author: 'Solo 2',
+    title: 'Second book',
+  },
+  {
+    id: '3',
+    author: 'Solo 3',
+    title: 'Third book',
+  },
+];
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return {
+      return [
         ...state,
-        books: state.books.concat(action.payload),
-      };
+        action.payload,
+      ];
     case REMOVE_BOOK:
-      return {
+      return [
         ...state,
-        books: state.books.filter((book) => book !== action.payload),
-      };
+        state.filter((book) => book !== action.payload),
+      ];
     default: return state;
   }
 };
 
-export const addBooks = (book) => ({
+export const addBook = (book) => ({
   type: ADD_BOOK,
   payload: book,
 });
